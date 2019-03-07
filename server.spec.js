064bb534e05e9dea3e4a82d38009d3371ae66db6
Angel Torres <alas4224@gmail.com>
2019-03-07 16:07:43 -0600

    begin adding tests to server.spec.js

---
+++ server.spec.js
@@ -0,0 +1,9 @@
+const request = require('supertest');
+const db = require('./data/dbConfig')
+const server = require('./server.js');
+
+describe('server', () => {
+    it('should get a list of students', async () => {
+        
+    })
+})
